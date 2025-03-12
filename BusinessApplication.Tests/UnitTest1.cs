cfca8bf7679c2f8b84482d66fe107b303b867aa5
sandeep16b <sandeep16b@gmail.com>
2025-03-12 02:14:25 -0400

    added tests

---
+++ BusinessApplication.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+using BusinessApplication; 
+
+public class SampleTests
+{
+    [Fact]
+    public void SampleTest_Passes()
+    {
+        int result = 2 + 3;
+        Assert.Equal(5, result);
+    }
+}
