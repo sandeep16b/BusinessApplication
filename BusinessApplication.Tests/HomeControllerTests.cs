e1535e6fcd869bed794f46945828a27e9911bf4d
sandeep16b <sandeep16b@gmail.com>
2025-03-21 00:07:58 -0400

    added one more unit test case

---
+++ BusinessApplication.Tests/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using BusinessApplication; 
+
+public class HomeControllerTests
+{
+    [Fact]
+    public void Index_Success()
+    {
+        int result = 2 + 3;
+        Assert.Equal(5, result);
+    }
+    [Fact]
+    public void Index_Fail()
+    {
+        int result = 2 + 3;
+        Assert.NotEqual(4, result);
+    }
+
+}
